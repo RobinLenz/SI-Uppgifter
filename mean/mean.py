308117b1ce56ec54665ae709b5d26dd2264348fd
Robin Lenz <robin.lenz@live.se>
2019-11-08 09:16:11 +0100

    Python program fixed

--- mean/mean.py
+++ mean/mean.py
@@ -1,36 +1,25 @@
-
-
-
 def main():
-    print("Hello World!")
-    tal = int(input("Enter number, 0 to exit\n"))
-    mini = tal
-    maxi = tal
+    number = int(input("Enter number, 0 to exit\n"))
+    mini = number
+    maxi = number
     total = int(0)
     number_of_numbers = int(0)
     
 
-    while not tal == 0:
-        if tal > maxi:
-            maxi = tal
-        elif tal < mini:
-            mini = tal
-        total += tal 
+    while not number == 0:
+        if number > maxi:
+            maxi = number
+        elif number < mini:
+            mini = number
+        total += number 
         number_of_numbers += 1   
-        tal = int(input("Enter number, 0 to exit\n"))
+        number = int(input("Enter number, 0 to exit\n"))
 
     total -= mini
     total -= maxi
     number_of_numbers -= 2
 
     print(f"The mean, excluding the largest and the smallest number is: {total/number_of_numbers}")
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
